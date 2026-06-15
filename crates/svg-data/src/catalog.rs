6822ed7de406fe09e64ad55ade366156e15ce16d
Kaj Kowalski <info@kajkowalski.nl>
2026-06-15 17:52:46 +0200

    start from scratch. don't look back asshole

--- crates/svg-data/src/catalog.rs
+++
@@ -1,7 +0,0 @@
-use crate::types::{
-    AttributeDef, AttributeValues, BaselineQualifier, BaselineStatus, BrowserSupport,
-    BrowserVersion, CompatVerdict, ContentModel, ElementCategory, ElementDef, RawVersionAdded,
-    SpecLifecycle, SpecSnapshotId, VerdictReason, VerdictRecommendation,
-};
-
-include!(concat!(env!("OUT_DIR"), "/catalog.rs"));
